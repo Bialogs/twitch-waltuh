89ee22cde45eb001137142736a14469f95fd9df8
Kyle Fagan <bialogs@gmail.com>
2023-06-05 13:53:33 -0700

    WIP Additional TLS config testing

--- server/app.rb
+++ server/app.rb
@@ -10,14 +10,25 @@ set :port, 4567
 set :bind, proc { ENV['development'] ? 'localhost' : '0.0.0.0' }
 
 if ENV['TW_TLS_ENABLED']
-  MyApp.run! do |server|
-    ssl_options = {
-      cert_chain_file: ENV['TW_SERVER_CHAIN_PATH'],
-      private_key_file: ENV['TW_SERVER_KEY_PATH'],
-      verify_peer: false
-    }
-    server.ssl = true
-    server.ssl_options = ssl_options
+  class MyThinBackend < ::Thin::Backends::TcpServer
+    def initialize(host, port, options)
+      super(host, port)
+      @ssl = true
+      @ssl_options = options
+    end
+  end
+
+  configure do
+    class << settings
+      def server_settings
+        {
+          backend: MyThinBackend,
+          private_key_file: ENV['TW_SERVER_KEY_PATH'],
+          cert_chain_file: ENV['TW_SERVER_CHAIN_PATH'],
+          verify_peer: false
+        }
+      end
+    end
   end
 end
 
