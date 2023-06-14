5f37b01a2e1cd1e202fa75bb625dbe4ccc05e731
Kyle Fagan <bialogs@gmail.com>
2023-06-13 23:09:41 -0700

    six handler placeholder

---
+++ lib/tw/handlers/six.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+require_relative ''
+
+module Tw
+  module Handlers
+    # Send a command when a random dice roll lands on 6
+    class Six
+      def initialize
+      end
+
+      def operation(message)
+      end
+
+      def callback(message, player)
+      end
+    end
+  end
+end
