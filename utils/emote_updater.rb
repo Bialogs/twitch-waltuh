be520f5c08039e1cb644a4f5de7989602debbe7b
Kyle Fagan <bialogs@gmail.com>
2023-06-27 20:40:24 -0700

    Remove debug statement

--- utils/emote_updater.rb
+++ utils/emote_updater.rb
@@ -17,7 +17,6 @@ global_emotes = JSON.parse(Net::HTTP.post(uri, request_content, headers).body)
 
 request_content = JSON.dump({"operationName":"GetEmoteSet","variables":{"id": channel_id},"query":"query GetEmoteSet($id: ObjectID!, $formats: [ImageFormat!]) {\n  emoteSet(id: $id) {\n    id\n    name\n    flags\n    capacity\n    origins {\n      id\n      weight\n      __typename\n    }\n    emotes {\n      id\n      name\n      actor {\n        id\n        display_name\n        avatar_url\n        __typename\n      }\n      origin_id\n      data {\n        id\n        name\n        flags\n        state\n        lifecycle\n        host {\n          url\n          files(formats: $formats) {\n            name\n            format\n            __typename\n          }\n          __typename\n        }\n        owner {\n          id\n          display_name\n          style {\n            color\n            __typename\n          }\n          roles\n          __typename\n        }\n        __typename\n      }\n      __typename\n    }\n    owner {\n      id\n      username\n      display_name\n      style {\n        color\n        __typename\n      }\n      avatar_url\n      roles\n      connections {\n        id\n        display_name\n        emote_capacity\n        platform\n        __typename\n      }\n      __typename\n    }\n    __typename\n  }\n}"})
 channel_emotes = JSON.parse(Net::HTTP.post(uri, request_content, headers).body)
-# require 'pry'; binding.pry
 
 emote_names = []
 emote_names.push(global_emotes["data"]["globalEmoteSet"]["emotes"].map { |emote| emote["name"] })
